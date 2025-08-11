69c2a77af56c6de4e3c9095ed9f5d07fee95229b
Tao Liu <tao.liu@ibm.com>
2025-08-11 18:05:24 -0400

    updatre

--- closed/src/java.base/share/classes/openj9/internal/security/RestrictedSecurity.java
+++ closed/src/java.base/share/classes/openj9/internal/security/RestrictedSecurity.java
@@ -44,6 +44,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
